7c99e1be914c232554a95cb3ebda3402c2123cea
antichristts <hamidovadam209@icloud.com>
2023-03-25 19:25:05 +0500

    Form callback

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Введите Ваше Имя">
+        <label>Tel</label>
+        <input type="tel" name="tel" placeholder="Введите ваш номер телфона">
+        <label>message</label>
+        <textarea row="10" cols="45" name="msg" placeholder="Введите обращение"></textarea>
+        <input type="submit" name="submit" placeholder="Отправить">
+    </form>
+</body>
+</html>
